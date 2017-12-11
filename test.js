00b75a5436e8a3cb745cec7c8f66e2fb72feb49b
androidgrl <kawaharajamie@gmail.com>
2017-12-10 22:18:02 -0800

    Fix indentation

--- test.js
+++ test.js
@@ -3,22 +3,22 @@ const _ = require('lodash@4.8.2');
 
 const channelIds = (ctx) => {
   return new Promise (function (fulfill, reject){
-      request.get("https://slack.com/api/channels.list", {
-            'auth': {
-                    'bearer': ctx.secrets.bearer
-                  }
-          }, function (err, res, body){
-                channels = JSON.parse(body).channels;
-                ids = _.map(channels, function (c) {
-                        return c.id;
-                      });
-                fulfill(ids);
-              });
+    request.get("https://slack.com/api/channels.list", {
+      'auth': {
+        'bearer': ctx.secrets.bearer
+      }
+    }, function (err, res, body){
+      channels = JSON.parse(body).channels;
+      ids = _.map(channels, function (c) {
+        return c.id;
+      });
+      fulfill(ids);
     });
+  });
 };
 
 module.exports = (ctx, cb) => {
   channelIds(ctx).then(function (result){
-      cb(null, {text: `${result}`});
-    });
+    cb(null, {text: `${result}`});
+  });
 };
