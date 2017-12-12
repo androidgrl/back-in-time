46650dc5f8d3508729ab4ec0361e66d2a40f8ef7
androidgrl <kawaharajamie@gmail.com>
2017-12-11 16:10:02 -0800

    Get the last message from each channel
    Use Promise.all to asynchronously fulfill an array of promise requests.
    Have to pass ctx in with each result since promises are only supposed to
    return one result.

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const _ = require('lodash@4.8.2');
 
-const channelIds = (ctx) => {
+const getChannelIds = (ctx) => {
   return new Promise (function (fulfill, reject){
     request.get("https://slack.com/api/channels.list", {
       'auth': {
@@ -9,16 +9,36 @@ const channelIds = (ctx) => {
       }
     }, function (err, res, body){
       channels = JSON.parse(body).channels;
-      ids = _.map(channels, function (c) {
-        return c.id;
+      idsWithCtx = _.map(channels, function (c) {
+        return {id: c.id, ctx: ctx};
       });
-      fulfill(ids);
+      fulfill(idsWithCtx);
     });
   });
 };
 
-module.exports = (ctx, cb) => {
-  channelIds(ctx).then(function (result){
-    cb(null, {text: `${result}`});
+const makeChannelRequest = (idwithCtx) => {
+  return new Promise (function (fulfill, reject) {
+    request.get('https://slack.com/api/conversations.history?channel=' + idwithCtx.id, {
+        'auth': {
+          'bearer': idwithCtx.ctx.secrets.bearer
+        }
+      }, function (err, res, body) {
+      lastMessage = JSON.parse(body).messages[0].text;
+      fulfill(lastMessage);
+    });
   });
+};
+
+const getLastMessages = (idsWithCtx) => {
+  channelRequests = _.map(idsWithCtx, makeChannelRequest);
+  return Promise.all(channelRequests);
+};
+
+module.exports = (ctx, cb) => {
+  getChannelIds(ctx)
+    .then(getLastMessages)
+    .then(function (result){
+      cb(null, {text: `${result}`});
+    });
 };
